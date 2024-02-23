0dcf39df777eb73b2cbbb757eb697ca38374b22b
RajasekarR1 <159535581+RajasekarR1@users.noreply.github.com>
2024-02-23 12:25:28 +0530

    Delete week3 directory

--- week3/day1/SBI_Bank.java
+++
@@ -1,9 +0,0 @@
-package week3.day1;
-
-public class SBI_Bank extends RBI_MethodOverride {
-	public int getRateofInterest() {
-		System.out.println();
-		return 8;
-
-	}
-}
--- week3/day2/DatabaseConnection_Assignment.java
+++
@@ -1,8 +0,0 @@
-package week3.day2;
-
-public interface DatabaseConnection_Assignment {
-		public void connect();
-		public void disconnect();
-		public	void executeUpdate();
-			
-}
